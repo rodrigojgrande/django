1e290f57c642e1fa77eb009c3810c49726f9b9f9
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-07 16:53:11 -0300

    Aula 31 - Arquivos estáticos no Django: CSS, JavaScript e Imagens

--- django1/core/views.py
+++ django1/core/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from django.http import HttpResponse
+from django.template import loader
 
 from .models import Produto
 
@@ -19,10 +23,20 @@ def contato(request):
 
 
 def produto(request, pk):
-    prod = Produto.objects.get(id=pk)
+    # prod = Produto.objects.get(id=pk)
+    prod = get_object_or_404(Produto, id=pk)
 
     context = {
         'produto': prod
     }
     return render(request, 'produto.html', context)
 
+
+def error404(request, exception):
+    template = loader.get_template('404.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=404)
+
+
+def error500(request):
+    template = loader.get_template('500.html')
+    return HttpResponse(content=template.render(), content_type='text/html; charset=utf8', status=500)
--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -118,7 +118,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static/'  # Usado durante o desenvolvimento
+STATIC_ROOT = BASE_DIR / 'staticfiles'  # Usado durante a produção
+
+LOGOUT_REDIRECT_URL = 'index'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
