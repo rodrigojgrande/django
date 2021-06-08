5280361a70cf8ba5d2d209f4ddd21a0102541219
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-08 10:44:23 -0300

    Novos arquivos Heroku

--- django1/django1/settings.py
+++ django1/django1/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ybt^4y5k1z)y9#t%v03iklh0jk*m)m9$lun2y3oi7ekqk^uj&i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']  # www.meusite.com.br
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
