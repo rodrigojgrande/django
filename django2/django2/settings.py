bf0f33476bb1eb01e12e4ac238a4872e8aa5d123
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-09 17:00:35 -0300

    Aula 38 - Definindo e configurando as rotas

--- django2/django2/settings.py
+++ django2/django2/settings.py
@@ -40,12 +40,12 @@ INSTALLED_APPS = [
 
     'core',
     'bootstrap4',
-    'stdimages',
+    'stdimage',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
