0cbc7ba263d7e44e4d231e89bf405d6515182e9f
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-11 14:31:49 -0300

    Aula 42 - Trabalhando com Formulários pt 2 - ModelForms

--- django2/core/forms.py
+++ django2/core/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.core.mail.message import EmailMessage
 
+from .models import Produto
+
 
 class ContatoForm(forms.Form):
     nome = forms.CharField(label='Nome', max_length=100)
@@ -24,3 +26,9 @@ class ContatoForm(forms.Form):
             headers={'Reply-To': email}
         )
         mail.send()
+
+
+class ProdutoModelForm(forms.ModelForm):
+    class Meta:
+        model = Produto
+        fields = ['nome', 'preco', 'estoque', 'imagem']
--- django2/core/views.py
+++ django2/core/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 
-from .forms import ContatoForm
+from .forms import ContatoForm, ProdutoModelForm
 
 
 def index(request):
@@ -26,4 +26,24 @@ def contato(request):
 
 
 def produto(request):
-    return render(request, 'produto.html')
+    if str(request.method) == 'POST':
+        form = ProdutoModelForm(request.POST, request.FILES)
+        if form.is_valid():
+            prod = form.save(commit=False)
+
+            print(f'Nome: {prod.nome}')
+            print(f'Preço: {prod.preco}')
+            print(f'Estoque: {prod.estoque}')
+            print(f'Nome: {prod.imagem}')
+
+            messages.success(request, 'Produto salvo com sucesso.')
+            form = ProdutoModelForm()
+        else:
+            messages.error(request, 'Erro ao salvar produto.')
+    else:
+        form = ProdutoModelForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'produto.html', context)
+
