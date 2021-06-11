3a31645da0f3000998640334f1b86a8649eb1f7f
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-11 17:31:21 -0300

    Aula 45 - Trabalhando com seções do usuário

--- django2/core/views.py
+++ django2/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.contrib import messages
+from django.shortcuts import redirect
 
 from .forms import ContatoForm, ProdutoModelForm
 from .models import Produto
@@ -30,19 +31,22 @@ def contato(request):
 
 
 def produto(request):
-    if str(request.method) == 'POST':
-        form = ProdutoModelForm(request.POST, request.FILES)
-        if form.is_valid():
+    if str(request.user) != 'AnonymousUser':
+        if str(request.method) == 'POST':
+            form = ProdutoModelForm(request.POST, request.FILES)
+            if form.is_valid():
 
-            form.save()
+                form.save()
 
-            messages.success(request, 'Produto salvo com sucesso.')
-            form = ProdutoModelForm()
+                messages.success(request, 'Produto salvo com sucesso.')
+                form = ProdutoModelForm()
+            else:
+                messages.error(request, 'Erro ao salvar produto.')
         else:
-            messages.error(request, 'Erro ao salvar produto.')
+            form = ProdutoModelForm()
+        context = {
+            'form': form
+        }
+        return render(request, 'produto.html', context)
     else:
-        form = ProdutoModelForm()
-    context = {
-        'form': form
-    }
-    return render(request, 'produto.html', context)
+        return redirect('index')
