bb9f67e92741776118c2c834e6d21f571ef0d7d9
Rodrigo Grande <rodrigojgrande@gmail.com>
2021-06-28 15:28:51 -0300

    Aula 81 - Relacionamento Um para Muitos

--- djangoorm/core/admin.py
+++ djangoorm/core/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Chassi, Carro
+from .models import Chassi, Carro, Montadora
+
+
+@admin.register(Montadora)
+class MontadoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', )
 
 
 @admin.register(Chassi)
@@ -10,4 +15,4 @@ class ChassiAdmin(admin.ModelAdmin):
 
 @admin.register(Carro)
 class CarroAdmin(admin.ModelAdmin):
-    list_display = ('modelo', 'chassi', 'preco')
+    list_display = ('montadora', 'modelo', 'chassi', 'preco')
--- djangoorm/core/models.py
+++ djangoorm/core/models.py
@@ -12,12 +12,28 @@ class Chassi(models.Model):
         return self.numero
 
 
+class Montadora(models.Model):
+    nome = models.CharField('Nome', max_length=50)
+
+    verbose_name = 'Montadora'
+    verbose_name_plural = 'Montadora'
+
+    def __str__(self):
+        return self.nome
+
+
 class Carro(models.Model):
     """
+    # OneToOneField
     Cada carro só pode se relacionar com um chassi
     e cada chassi só pode se relacionar com um carro
+
+    #ForeignKey (One to Many)
+    Cada carro tem uma montadora mas uma montadora
+    pode 'montar' varios carros
     """
     chassi = models.OneToOneField(Chassi, on_delete=models.CASCADE)
+    montadora = models.ForeignKey(Montadora, on_delete=models.CASCADE)
     modelo = models.CharField('Modelo', max_length=30, help_text='Máximo 30 caracteres')
     preco = models.DecimalField('Preco', max_digits=8, decimal_places=2)
 
@@ -26,4 +42,4 @@ class Carro(models.Model):
         verbose_name_plural = 'Carros'
 
     def __str__(self):
-        return self.modelo
+        return f'{self.montadora} {self.modelo}'
